01d8ebd5b913c68e6698347dd70e835ce6709542
kocerenes <eneskocerr41@gmail.com>
2022-10-07 23:56:56 +0300

    Create the navigation and fragments

---
+++ app/src/main/java/com/enesk/foodrecipes/presentation/favorite_recipes/FavoriteRecipesFragment.kt
@@ -0,0 +1,24 @@
+package com.enesk.foodrecipes.presentation.favorite_recipes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.enesk.foodrecipes.R
+
+class FavoriteRecipesFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+    }
+
+}
