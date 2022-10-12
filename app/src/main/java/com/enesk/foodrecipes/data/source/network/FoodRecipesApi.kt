66aa04d142983a75177d8bd72d7795f6ada92edd
kocerenes <eneskocerr41@gmail.com>
2022-10-12 19:03:14 +0300

    Update the RecipesRowBinding

--- app/src/main/java/com/enesk/foodrecipes/data/source/network/FoodRecipesApi.kt
+++ app/src/main/java/com/enesk/foodrecipes/data/source/network/FoodRecipesApi.kt
@@ -9,11 +9,13 @@ interface FoodRecipesApi {
 
     //https://api.spoonacular.com/recipes/complexSearch?number=50&apiKey=afc441cb71344918b659bff0a54cc71b&type=snack&diet=vegan&addRecipeInformation=true&fillIngredients=true
 
-    //https://api.spoonacular.com/recipes/complexSearch
+    //https://api.spoonacular.com/recipes/
+    // complexSearch
     // ?number=50
     // &apiKey=afc441cb71344918b659bff0a54cc71b
     // &type=snack
-    // &diet=vegan&addRecipeInformation=true
+    // &diet=vegan
+    // &addRecipeInformation=true
     // &fillIngredients=true
     @GET("/recipes/complexSearch")
     suspend fun getRecipes(
