156ffc85c6c5243323c283a274bfc30047c1ecab
kocerenes <eneskocerr41@gmail.com>
2022-10-13 14:28:58 +0300

    Create the RecipesFragment

--- app/src/main/java/com/enesk/foodrecipes/presentation/recipes/RecipesFragment.kt
+++ app/src/main/java/com/enesk/foodrecipes/presentation/recipes/RecipesFragment.kt
@@ -1,26 +1,84 @@
 package com.enesk.foodrecipes.presentation.recipes
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.enesk.foodrecipes.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.enesk.foodrecipes.databinding.FragmentRecipesBinding
+import com.enesk.foodrecipes.util.Constants.API_KEY
+import com.enesk.foodrecipes.util.NetworkResult
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class RecipesFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
 
-    }
+    private val recipesAdapter by lazy { RecipesAdapter() }
+    private val recipesViewModel: RecipesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+
+        setupRecyclerView()
+        requestApiData()
+
+        return binding.root
     }
 
+    private fun requestApiData() {
+        recipesViewModel.getRecipes(applyQueries())
+        recipesViewModel.recipesResponse.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is NetworkResult.Success -> {
+                    response.data?.let {
+                        recipesAdapter.setData(newData = it)
+                    }
+                }
+                is NetworkResult.Error -> {
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is NetworkResult.Loading -> {
+                    //TODO: progressbar
+                }
+            }
+        }
+    }
+
+    private fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+
+        queries["number"] = "50"
+        queries["apiKey"] = API_KEY
+        queries["type"] = "snack"
+        queries["diet"] = "vegan"
+        queries["addRecipeInformation"] = "true"
+        queries["fillIngredients"] = "true"
+
+        return queries
+    }
+
+    private fun setupRecyclerView() {
+        with(binding) {
+            recipesRecyclerView.adapter = recipesAdapter
+            recipesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
