2f0a731e62065d7fa6e15195a02c9f6fc54dead8
kocerenes <eneskocerr41@gmail.com>
2022-10-23 17:52:33 +0300

    Update the FoodRecipesApi for searchRecipe

--- app/src/main/java/com/enesk/foodrecipes/data/source/network/FoodRecipesApi.kt
+++ app/src/main/java/com/enesk/foodrecipes/data/source/network/FoodRecipesApi.kt
@@ -21,4 +21,9 @@ interface FoodRecipesApi {
     suspend fun getRecipes(
         @QueryMap queries: Map<String, String>
     ): Response<FoodRecipe>
+
+    @GET("/recipes/complexSearch")
+    suspend fun searchRecipes(
+        @QueryMap searchQuery: Map<String, String>
+    ): Response<FoodRecipe>
 }
