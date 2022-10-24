ef7803b4ff5950ebb4698c003f4399c55b9ee57d
kocerenes <eneskocerr41@gmail.com>
2022-10-24 12:54:42 +0300

    Create the DetailsActivity

---
+++ app/src/main/java/com/enesk/foodrecipes/presentation/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.enesk.foodrecipes.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.enesk.foodrecipes.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
