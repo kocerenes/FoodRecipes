6cf751ada48f200d06d3c55e7b7f29655ed925b7
kocerenes <eneskocerr41@gmail.com>
2022-10-27 12:33:50 +0300

    Create the fragments in DetailsScreen

---
+++ app/src/main/java/com/enesk/foodrecipes/presentation/details_screens/overview/OverviewFragment.kt
@@ -0,0 +1,24 @@
+package com.enesk.foodrecipes.presentation.details_screens.overview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.enesk.foodrecipes.R
+
+class OverviewFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_overview, container, false)
+    }
+}
