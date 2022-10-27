6098cc6dcf26691408e1e488b42b8f5500ece968
kocerenes <eneskocerr41@gmail.com>
2022-10-27 11:21:56 +0300

    Bug fixed

--- app/src/main/java/com/enesk/foodrecipes/data/source/network/model/ExtendedIngredient.kt
+++ app/src/main/java/com/enesk/foodrecipes/data/source/network/model/ExtendedIngredient.kt
@@ -1,21 +1,24 @@
 package com.enesk.foodrecipes.data.source.network.model
 
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class ExtendedIngredient(
     @SerializedName("amount")
-    val amount: Double,
+    val amount: Double?,
     @SerializedName("consistency")
-    val consistency: String,
+    val consistency: String?,
     @SerializedName("image")
-    val image: String,
+    val image: String?,
     @SerializedName("name")
-    val name: String,
+    val name: String?,
     @SerializedName("nameClean")
-    val nameClean: String,
+    val nameClean: String?,
     @SerializedName("original")
-    val original: String,
+    val original: String?,
     @SerializedName("unit")
-    val unit: String
-)
+    val unit: String?
+):Parcelable
