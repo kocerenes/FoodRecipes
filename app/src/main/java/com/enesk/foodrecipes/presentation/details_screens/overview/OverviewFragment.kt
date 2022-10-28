21a1365adee11a12d37b207a41293b6a30ef9c4b
kocerenes <eneskocerr41@gmail.com>
2022-10-28 11:51:16 +0300

    Create the display recipe detail data in OverviewFragment

--- app/src/main/java/com/enesk/foodrecipes/presentation/details_screens/overview/OverviewFragment.kt
+++ app/src/main/java/com/enesk/foodrecipes/presentation/details_screens/overview/OverviewFragment.kt
@@ -5,20 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import coil.load
 import com.enesk.foodrecipes.R
+import com.enesk.foodrecipes.data.source.network.model.Result
+import com.enesk.foodrecipes.databinding.FragmentOverviewBinding
 
 class OverviewFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var _binding: FragmentOverviewBinding
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        _binding = FragmentOverviewBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable("recipeBundle")
+
+        displayDataOnScreen(myBundle = myBundle)
+
+        return binding.root
+    }
+
+    private fun displayDataOnScreen(myBundle: Result?) {
+        myBundle?.let { result ->
+            with(binding) {
+                mainImageView.load(result.image)
+                titleTextView.text = result.title
+                likesTextView.text = result.aggregateLikes.toString()
+                timeTextView.text = result.readyInMinutes.toString()
+                summaryTextView.text = result.summary
+
+                result.vegetarian?.let { setColorOfIcon(it,vegetarianImageView,vegetarianTextView) }
+                result.vegan?.let { setColorOfIcon(it,veganImageView,veganTextView) }
+                result.glutenFree?.let { setColorOfIcon(it,glutenFreeImageView,glutenFreeTextView) }
+                result.dairyFree?.let { setColorOfIcon(it,dairyFreeImageView,dairyFreeTextView) }
+                result.veryHealthy?.let { setColorOfIcon(it,healthyImageView,healthyTextView) }
+                result.cheap?.let { setColorOfIcon(it,cheapImageView,cheapTextView) }
+            }
+        }
+    }
+
+    private fun setColorOfIcon(result: Boolean, imageView: ImageView, textView: TextView) {
+        if (result) {
+            imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
     }
 }
