541925b98c2ee35adcda1d325b63b4b5d4c50354
kocerenes <eneskocerr41@gmail.com>
2022-10-30 18:53:23 +0300

    Create the InstructionsFragment for display data on webview

--- app/src/main/java/com/enesk/foodrecipes/presentation/details_screens/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/enesk/foodrecipes/presentation/details_screens/instructions/InstructionsFragment.kt
@@ -1,25 +1,40 @@
 package com.enesk.foodrecipes.presentation.details_screens.instructions
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.enesk.foodrecipes.R
-
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.enesk.foodrecipes.data.source.network.model.Result
+import com.enesk.foodrecipes.databinding.FragmentInstructionsBinding
+import com.enesk.foodrecipes.util.Constants.RECIPE_RESULT_KEY
 
 class InstructionsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(RECIPE_RESULT_KEY)
+
+        setupWebView(myBundle = myBundle)
+
+        return binding.root
+    }
+
+    private fun setupWebView(myBundle: Result?) {
+        with(binding) {
+            instructionsWebView.webViewClient = object : WebViewClient() {}
+            val websiteUrl = myBundle?.sourceUrl
+            websiteUrl?.let { instructionsWebView.loadUrl(websiteUrl) }
+        }
     }
 }
