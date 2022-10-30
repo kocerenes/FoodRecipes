47165e66fd81bcc3b4f38578c5c204abfb255eb9
kocerenes <eneskocerr41@gmail.com>
2022-10-30 15:56:46 +0300

    Change the RecipesDiffUtil be generic type

--- app/src/main/java/com/enesk/foodrecipes/util/RecipesDiffUtil.kt
+++ app/src/main/java/com/enesk/foodrecipes/util/RecipesDiffUtil.kt
@@ -3,9 +3,9 @@ package com.enesk.foodrecipes.util
 import androidx.recyclerview.widget.DiffUtil
 import com.enesk.foodrecipes.data.source.network.model.Result
 
-class RecipesDiffUtil(
-    private val oldList: List<Result>,
-    private val newList: List<Result>
+class RecipesDiffUtil<T>(
+    private val oldList: List<T>,
+    private val newList: List<T>
 ) : DiffUtil.Callback() {
 
     override fun getOldListSize(): Int = oldList.size
