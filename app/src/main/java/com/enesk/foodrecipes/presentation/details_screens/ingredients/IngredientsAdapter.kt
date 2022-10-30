d6dd0a743ace6d7da03f889270fef87feca8caa9
kocerenes <eneskocerr41@gmail.com>
2022-10-30 15:57:47 +0300

    Display the ingredients on IngredientsFragment

---
+++ app/src/main/java/com/enesk/foodrecipes/presentation/details_screens/ingredients/IngredientsAdapter.kt
@@ -0,0 +1,52 @@
+package com.enesk.foodrecipes.presentation.details_screens.ingredients
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.enesk.foodrecipes.R
+import com.enesk.foodrecipes.data.source.network.model.ExtendedIngredient
+import com.enesk.foodrecipes.databinding.IngredientsRowLayoutBinding
+import com.enesk.foodrecipes.util.Constants.BASE_IMAGE_URL
+import com.enesk.foodrecipes.util.RecipesDiffUtil
+
+class IngredientsAdapter : RecyclerView.Adapter<IngredientsAdapter.MyViewHolder>() {
+
+    private var ingredientsList = emptyList<ExtendedIngredient>()
+
+    class MyViewHolder(val binding: IngredientsRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = IngredientsRowLayoutBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        with(holder) {
+            with(ingredientsList[position]) {
+                binding.ingredientImageView.load(BASE_IMAGE_URL + image) {
+                    crossfade(600)
+                    error(R.drawable.ic_error_placeholder)
+                }
+                name?.let { binding.ingredientNameTextView.text = it.capitalize() }
+                binding.ingredientAmountTextView.text = amount.toString()
+                binding.ingredientUnitTextView.text = unit
+                binding.ingredientConsistencyTextView.text = consistency
+                binding.ingredientOriginalTextView.text = original
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = ingredientsList.size
+
+    fun setData(newIngredients: List<ExtendedIngredient>) {
+        val ingredientsDiffUtil = RecipesDiffUtil(ingredientsList, newIngredients)
+        val diffUtilResult = DiffUtil.calculateDiff(ingredientsDiffUtil)
+        ingredientsList = newIngredients
+        diffUtilResult.dispatchUpdatesTo(this)
+
+    }
+}
--- app/src/main/java/com/enesk/foodrecipes/presentation/details_screens/ingredients/IngredientsFragment.kt
+++ app/src/main/java/com/enesk/foodrecipes/presentation/details_screens/ingredients/IngredientsFragment.kt
@@ -1,14 +1,22 @@
 package com.enesk.foodrecipes.presentation.details_screens.ingredients
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.enesk.foodrecipes.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.enesk.foodrecipes.data.source.network.model.Result
+import com.enesk.foodrecipes.databinding.FragmentIngredientsBinding
+import com.enesk.foodrecipes.util.Constants.RECIPE_RESULT_KEY
 
 
 class IngredientsFragment : Fragment() {
+
+    private var _binding: FragmentIngredientsBinding? = null
+    private val binding get() = _binding!!
+
+    private val ingredientsAdapter: IngredientsAdapter by lazy { IngredientsAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +27,21 @@ class IngredientsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ingredients, container, false)
+        _binding = FragmentIngredientsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(RECIPE_RESULT_KEY)
+
+        setupRecyclerView()
+        myBundle?.extendedIngredients?.let { ingredients ->
+            ingredientsAdapter.setData(ingredients)
+        }
+
+        return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        binding.ingredientsRecyclerview.adapter = ingredientsAdapter
+        binding.ingredientsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
     }
 }
