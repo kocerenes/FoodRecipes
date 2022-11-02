7faaae8f4b810a88b415cc54cfb400bc2f6f1a0b
kocerenes <eneskocerr41@gmail.com>
2022-11-02 10:26:22 +0300

    Modify the RecipesTypeConverter

--- app/src/main/java/com/enesk/foodrecipes/data/util/RecipesTypeConverter.kt
+++ app/src/main/java/com/enesk/foodrecipes/data/util/RecipesTypeConverter.kt
@@ -2,6 +2,7 @@ package com.enesk.foodrecipes.data.util
 
 import androidx.room.TypeConverter
 import com.enesk.foodrecipes.data.source.network.model.FoodRecipe
+import com.enesk.foodrecipes.data.source.network.model.Result
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
@@ -18,5 +19,16 @@ class RecipesTypeConverter {
     fun stringToFoodRecipe(data: String): FoodRecipe {
         val listType = object : TypeToken<FoodRecipe>() {}.type
         return gson.fromJson(data, listType)
+    }
+
+    @TypeConverter
+    fun resultToString(result:Result): String{
+        return gson.toJson(result)
+    }
+
+    @TypeConverter
+    fun stringToResult(data: String): Result{
+        val listType = object : TypeToken<Result>(){}.type
+        return gson.fromJson(data,listType)
     }
 }
