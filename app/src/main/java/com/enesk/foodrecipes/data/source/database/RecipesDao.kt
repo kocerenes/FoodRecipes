08f35957235fc009b791f9d59ff78986eb40cfcb
kocerenes <eneskocerr41@gmail.com>
2022-11-02 10:37:08 +0300

    Modify the RecipesDao interface

--- app/src/main/java/com/enesk/foodrecipes/data/source/database/RecipesDao.kt
+++ app/src/main/java/com/enesk/foodrecipes/data/source/database/RecipesDao.kt
@@ -1,9 +1,11 @@
 package com.enesk.foodrecipes.data.source.database
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.enesk.foodrecipes.data.source.database.entity.FavoritesEntity
 import com.enesk.foodrecipes.data.source.database.entity.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -15,4 +17,17 @@ interface RecipesDao {
 
     @Query("SELECT * FROM recipes_table ORDER BY id ASC")
     fun readRecipes(): Flow<List<RecipesEntity>>
+
+    /** For Favorite Recipes **/
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("SELECT * FROM favorites_recipes_table ORDER BY id ASC")
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>>
+
+    @Delete
+    suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("DELETE FROM favorites_recipes_table")
+    suspend fun deleteAllFavoriteRecipes()
 }
