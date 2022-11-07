503e2f793b074e5058a7974d599619dbd561b503
kocerenes <eneskocerr41@gmail.com>
2022-11-07 13:45:07 +0300

    Create the FavoriteRecipesFragment

--- app/src/main/java/com/enesk/foodrecipes/presentation/favorite_recipes/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/enesk/foodrecipes/presentation/favorite_recipes/FavoriteRecipesFragment.kt
@@ -1,24 +1,47 @@
 package com.enesk.foodrecipes.presentation.favorite_recipes
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.enesk.foodrecipes.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.enesk.foodrecipes.databinding.FragmentFavoriteRecipesBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentFavoriteRecipesBinding? = null
+    private val binding get() = _binding!!
+
+    private val favoriteAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter() }
+    private val favoriteViewModel by viewModels<FavoriteRecipesViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+
+        _binding = FragmentFavoriteRecipesBinding.inflate(inflater, container, false)
+
+        binding.lifecycleOwner = this
+        binding.favoriteViewModel = favoriteViewModel
+        binding.favoriteAdapter = favoriteAdapter
+
+        setupRecyclerView()
+
+        return binding.root
     }
 
+    private fun setupRecyclerView() {
+        binding.favoriteRecipeRecyclerView.adapter = favoriteAdapter
+        binding.favoriteRecipeRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
