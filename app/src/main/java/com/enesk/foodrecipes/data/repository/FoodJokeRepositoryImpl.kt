d4fb4ba8ebeaa26715d9efea19c517d41987fd17
kocerenes <eneskocerr41@gmail.com>
2022-11-10 15:17:15 +0300

    Modify the FoodJokeRepository for local cache food joke

--- app/src/main/java/com/enesk/foodrecipes/data/repository/FoodJokeRepositoryImpl.kt
+++ app/src/main/java/com/enesk/foodrecipes/data/repository/FoodJokeRepositoryImpl.kt
@@ -1,16 +1,27 @@
 package com.enesk.foodrecipes.data.repository
 
+import com.enesk.foodrecipes.data.source.database.entity.FoodJokeEntity
+import com.enesk.foodrecipes.data.source.local.LocalDataSource
 import com.enesk.foodrecipes.data.source.network.model.food_joke.FoodJoke
 import com.enesk.foodrecipes.data.source.remote.RemoteDataSource
 import com.enesk.foodrecipes.domain.repository.FoodJokeRepository
+import kotlinx.coroutines.flow.Flow
 import retrofit2.Response
 import javax.inject.Inject
 
 class FoodJokeRepositoryImpl @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource
 ) : FoodJokeRepository {
 
     override suspend fun getFoodJoke(apiKey: String): Response<FoodJoke> {
         return remoteDataSource.getFoodJoke(apiKey = apiKey)
+    }
+
+    override fun readFoodJoke(): Flow<List<FoodJokeEntity>> =
+        localDataSource.readFoodJoke()
+
+    override suspend fun insertFoodJoke(foodJokeEntity: FoodJokeEntity) {
+        localDataSource.insertFoodJoke(foodJokeEntity = foodJokeEntity)
     }
 }
--- app/src/main/java/com/enesk/foodrecipes/domain/repository/FoodJokeRepository.kt
+++ app/src/main/java/com/enesk/foodrecipes/domain/repository/FoodJokeRepository.kt
@@ -1,9 +1,13 @@
 package com.enesk.foodrecipes.domain.repository
 
+import com.enesk.foodrecipes.data.source.database.entity.FoodJokeEntity
 import com.enesk.foodrecipes.data.source.network.model.food_joke.FoodJoke
+import kotlinx.coroutines.flow.Flow
 import retrofit2.Response
 
 interface FoodJokeRepository {
 
     suspend fun getFoodJoke(apiKey: String): Response<FoodJoke>
+    fun readFoodJoke(): Flow<List<FoodJokeEntity>>
+    suspend fun insertFoodJoke(foodJokeEntity: FoodJokeEntity)
 }
