af4be75bf1dd06339568fef37ba65e3ff1a609f1
kocerenes <eneskocerr41@gmail.com>
2022-11-12 11:32:48 +0300

    Create the share menu

--- app/src/main/java/com/enesk/foodrecipes/presentation/main_screens/food_joke/FoodJokeFragment.kt
+++ app/src/main/java/com/enesk/foodrecipes/presentation/main_screens/food_joke/FoodJokeFragment.kt
@@ -1,14 +1,22 @@
 package com.enesk.foodrecipes.presentation.main_screens.food_joke
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import com.enesk.foodrecipes.R
 import com.enesk.foodrecipes.databinding.FragmentFoodJokeBinding
 import com.enesk.foodrecipes.util.NetworkResult
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,12 +24,14 @@ import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
-class FoodJokeFragment : Fragment() {
+class FoodJokeFragment : Fragment(), MenuProvider {
 
     private val foodJokeViewModel by viewModels<FoodJokeViewModel>()
 
     private var _binding: FragmentFoodJokeBinding? = null
     private val binding get() = _binding!!
+
+    private var sharedFoodJoke = "No Food Joke"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +41,9 @@ class FoodJokeFragment : Fragment() {
         _binding = FragmentFoodJokeBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
         binding.foodJokeViewModel = foodJokeViewModel
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         observeFoodJoke()
 
@@ -43,6 +56,7 @@ class FoodJokeFragment : Fragment() {
             when (response) {
                 is NetworkResult.Success -> {
                     binding.foodJokeTextView.text = response.data?.text
+                    sharedFoodJoke = response.data?.text.toString()
                 }
                 is NetworkResult.Loading -> {
                     Log.d("FoodJokeFragment", "Loading")
@@ -64,9 +78,26 @@ class FoodJokeFragment : Fragment() {
             foodJokeViewModel.readFoodJoke.observe(viewLifecycleOwner) { listFoodJoke ->
                 if (!listFoodJoke.isNullOrEmpty()) {
                     binding.foodJokeTextView.text = listFoodJoke[0].foodJoke.text
+                    sharedFoodJoke = listFoodJoke[0].foodJoke.text.toString()
                 }
             }
         }
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.food_joke_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        if (menuItem.itemId == R.id.share_food_joke_menu) {
+            val shareIntent = Intent().apply {
+                this.action = Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT, sharedFoodJoke)
+                this.type = "text/plain"
+            }
+            startActivity(shareIntent)
+        }
+        return true
     }
 
     override fun onDestroy() {
