3c3c632cb412ed27c44ec188b8d2499a25807c38
kocerenes <eneskocerr41@gmail.com>
2022-11-12 12:15:32 +0300

    Create the Splash Screen

--- app/src/main/java/com/enesk/foodrecipes/presentation/main_screens/MainActivity.kt
+++ app/src/main/java/com/enesk/foodrecipes/presentation/main_screens/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setTheme(R.style.Theme_FoodRecipes)
         setContentView(binding.root)
 
         val navHostFragment =
